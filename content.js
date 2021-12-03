7f2b4b98bb2bc3f16ef00ad55172b34f6a0e8f63
darutoko <darutoko@gmail.com>
2021-12-03 22:45:04 +0700

    cleanup

--- content.js
+++ content.js
@@ -37,30 +37,6 @@ function applyFilters() {
 		for (let pattern of filters.patterns) {
 			if ("regexp" in pattern && pattern.regexp.test(text)) {
 				hideMessageContent(message, "Фильтр: " + pattern.display)
-				return
-				// let filterBoxId = message.id + "Filter";
-				// Create button
-				let buttonToggleMessage = document.createElement("span")
-				// buttonToggleMessage.dataset.status = "hidden";
-				buttonToggleMessage.innerHTML = "&nbsp"
-				buttonToggleMessage.className = "fg-btn fg-btni-show-message"
-				// buttonToggleMessage.setAttribute("title", "Показать сообщение");
-				buttonToggleMessage.addEventListener("click", event => toggleMessage(event.target, message.id))
-				// Create filter display indicator
-				let filterDisplay = document.createElement("span")
-				filterDisplay.setAttribute("style", "padding-right: 10px;")
-				filterDisplay.append("Фильтр: " + pattern.display)
-				// Add filter display and button to DOM
-				let li = document.createElement("li")
-				li.append(buttonToggleMessage)
-				message.controls.prepend(li)
-				li = document.createElement("li")
-				li.append(filterDisplay)
-				message.controls.prepend(li)
-				// Hide message body
-				// messageContent.parentElement.setAttribute("style", "display: none;");
-				message.content.parentElement.classList.add("fg-hidden")
-				return;
 			}
 		}
 	})
@@ -127,13 +103,6 @@ function onToggleMessage(button, messageId) {
 	messageBody.classList.toggle("fg-hidden")
 	button.classList.toggle("fg-btni-show-message")
 	button.classList.toggle("fg-btni-hide-message")
-	// if (button.dataset.status === "hidden") {
-	// 	messageBody.classList.remove("fg-hidden");
-	// 	button.dataset.status = "shown";
-	// } else {
-	// 	messageBody.classList.add("fg-hidden");
-	// 	button.dataset.status = "hidden";
-	// }
 }
 
 function saveFilters() {
